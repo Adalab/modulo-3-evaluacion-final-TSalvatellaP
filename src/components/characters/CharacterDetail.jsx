4128147f38cc8af930749db3593e5570ca6e2621
Tania Salvatella <tsalvatellap@gmail.com>
2025-02-05 10:52:36 +0100

    house-coats

--- src/components/characters/CharacterDetail.jsx
+++ src/components/characters/CharacterDetail.jsx
@@ -1,6 +1,21 @@
 import PropTypes from "prop-types";
 
 function CharacterDetail({info}) {
+  const getHouseImage = (house) => {
+    if (house === "Gryffindor"){
+      return "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTyeGE0B7FFvyj9ZeSEbjQYXS1aL7_6x2fcMAVmEg1O0C0QMJEY6ctUEzTFqxFuxIpBoXnm6DVWbdo4qxExPoCp8Be9xy4_SNHbh6ZpikucaRgpUGgSJ22Fgoxeeb9ni7gV7ZWRrKBuWQ&usqp=CAc";
+    } else if (house === "Slytherin") {
+      return "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSJHeUE2hNviuUdtY5eMkJB-QMXwPatH00ekzE2iiseifQWq1KUBNVFVIXOI6-DIbsns7sLxqOeA0UM0OXAjAgJQYnp1P4CxEQZjtMHpE0jTq_j32Yq9Q0DOQFsgAelWhr311arzydj8o4&usqp=CAc";
+    } else if (house === "Ravenclaw"){
+      return "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQNVhkWKwpDLH2nMvVVIrKupomsAA76C_qBhIYpyT1kAhFnotttY9H4U2l9__nTtSc8D5nxUhI2PFNenq9Z2Iwq_hiQcCzLkRtCLdyRPiRRPk5r0YrPzOn6NLemSzTl0XpnS3JwsHgAcGo&usqp=CAc";
+    } else if (house === "Hufflepuff"){
+      return "https://media.mykaramelli.com/galeria/articulos/decoracion-de-pared-emblema-hufflepuff-harry-potter-61cm_12419_1.jpg";
+    } else {
+      return "/images/default.png";
+    }
+  }
+
+
   return (
     <div>
       <img src={info.image} alt={info.name} />
@@ -10,7 +25,9 @@ function CharacterDetail({info}) {
          </p>
       <p>Género: {info.gender}</p>
       <p>Especie: {info.species === "human" ? <i className="fa-solid fa-user"></i> : <i className="fa-brands fa-reddit-alien"></i>}</p>
-      <p>Casa: {info.house}</p>
+      <p>Casa: {info.house}
+      <img src={getHouseImage(info.house)} alt={`Escudo de ${info.house}`} />
+      </p>
     </div>
   )
 }
