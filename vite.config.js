3d21814590cb481aa5e4ad02809e66c70f98eda3
TSalvatellaP <tsalvatellap@gmail.com>
2025-02-06 12:22:20 +0100

    vite confing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nombre-repo/",
+  base: "/modulo-3-evaluacion-final-TSalvatellaP/",
   server:{
     watch:{
       usePolling: true,
