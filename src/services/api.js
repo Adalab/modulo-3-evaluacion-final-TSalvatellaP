87b62eff7dbf278225d196eb230bf8636906a58e
TSalvatellaP <tsalvatellap@gmail.com>
2025-02-06 12:27:18 +0100

    image issue

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,7 @@ function api (house){
                 id: character.id,
                 name: character.name,
                 species: character.species,
-                image: character.image || "/src/images/file_15140689.png",
+                image: character.image || "/images/file_15140689.png",
                 gender: character.gender,
                 house: character.house || "Sin casa",
                 alive: character.alive,
